29507cc2595dec752f5ca4ab6424e44d0d8ae916
Rilsikane <jutha.se@gmail.com>
2017-11-24 11:39:57 +0700

    for android

--- android/app/src/main/java/com/gable/mobilestaffio/MainApplication.java
+++ android/app/src/main/java/com/gable/mobilestaffio/MainApplication.java
@@ -3,6 +3,7 @@ package com.gable.mobilestaffio;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationApplication;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.ocetnik.timer.BackgroundTimerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -19,18 +20,56 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+//public class MainApplication extends Application implements ReactApplication {
+//
+//  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//    @Override
+//    public boolean getUseDeveloperSupport() {
+//      return BuildConfig.DEBUG;
+//    }
+//
+//    @Override
+//    protected List<ReactPackage> getPackages() {
+//      return Arrays.<ReactPackage>asList(
+//          new MainReactPackage(),
+//            new NavigationReactPackage(),
+//            new ImageResizerPackage(),
+//            new BackgroundTimerPackage(),
+//            new RNDeviceInfo(),
+//            new RNFSPackage(),
+//            new RCTCameraPackage(),
+//            new RNSpinkitPackage(),
+//            new SplashScreenReactPackage(),
+//            new FIRMessagingPackage(),
+//            new VectorIconsPackage()
+//      );
+//    }
+//  };
+//
+//  @Override
+//  public ReactNativeHost getReactNativeHost() {
+//    return mReactNativeHost;
+//  }
+//
+//  @Override
+//  public void onCreate() {
+//    super.onCreate();
+//    SoLoader.init(this, /* native exopackage */ false);
+//  }
+//}
+public class MainApplication extends NavigationApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public boolean isDebug() {
+        // Make sure you are using BuildConfig from your own application
+        return BuildConfig.DEBUG;
     }
 
-    @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
+        // Add additional packages you require here
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
             new ImageResizerPackage(),
             new BackgroundTimerPackage(),
             new RNDeviceInfo(),
@@ -40,18 +79,11 @@ public class MainApplication extends Application implements ReactApplication {
             new SplashScreenReactPackage(),
             new FIRMessagingPackage(),
             new VectorIconsPackage()
-      );
+        );
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
 }
