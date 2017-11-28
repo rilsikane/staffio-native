b6204e1dcc09dc08c9669231e1a052550f30a327
Pramot01 <parmot.s@g-able.com>
2017-11-28 16:05:17 +0700

    no message

--- android/app/src/main/java/com/gable/mobilestaffio/MainApplication.java
+++ android/app/src/main/java/com/gable/mobilestaffio/MainApplication.java
@@ -4,13 +4,14 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.reactnativenavigation.NavigationApplication;
+
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.ocetnik.timer.BackgroundTimerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.rnfs.RNFSPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
-
 import com.evollu.react.fcm.FIRMessagingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -20,17 +21,61 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+
+
+
+//public class MainApplication extends Application implements ReactApplication {
+//
+//  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//    @Override
+//    public boolean getUseDeveloperSupport() {
+//      return BuildConfig.DEBUG;
+//    }
+//
+//    @Override
+//    protected List<ReactPackage> getPackages() {
+//      return Arrays.<ReactPackage>asList(
+//          new MainReactPackage(),
+//            new NavigationReactPackage(),
+//            new ImageResizerPackage(),
+//            new BackgroundTimerPackage(),
+//            new RNDeviceInfo(),
+//            new RNFSPackage(),
+//            new RCTCameraPackage(),
+//            new RNSpinkitPackage(),
+//            new SplashScreenReactPackage(),
+//            new FIRMessagingPackage(),
+//            new VectorIconsPackage()
+//      );
+//    }
+//  };
+//
+//  @Override
+//  public ReactNativeHost getReactNativeHost() {
+//    return mReactNativeHost;
+//  }
+//
+//  @Override
+//  public void onCreate() {
+//    super.onCreate();
+//    SoLoader.init(this, /* native exopackage */ false);
+//  }
+//}
+
+
+public class MainApplication extends NavigationApplication  {
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public boolean isDebug() {
+        // Make sure you are using BuildConfig from your own application
+        return BuildConfig.DEBUG;
     }
 
-    @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+
+        return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new SplashScreenReactPackage(),
             new ImageResizerPackage(),
@@ -41,18 +86,18 @@ public class MainApplication extends Application implements ReactApplication {
             new RNSpinkitPackage(),
             new FIRMessagingPackage(),
             new VectorIconsPackage()
-      );
+        );
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
 }
+
+
+
+
+
+
+
