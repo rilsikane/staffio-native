081aa09943fe70a105c0161d5e1231129e70b38e
Milkk <sudarat5731305077@gmail.com>
2018-02-13 14:36:58 +0700

    no message

---
+++ src/utils/i18n.js
@@ -0,0 +1,15 @@
+import I18n from 'react-native-i18n';
+import { getLanguages } from 'react-native-i18n';
+
+import en from './locales/en';
+import th from './locales/th';
+
+
+// I18n.fallbacks = true;
+I18n.locale = 'th';
+I18n.translations = {
+  en,
+  th
+};
+
+export default I18n;
