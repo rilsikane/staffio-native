44eb17fe18ba35b4c0406fce10306755dddf0865
Rilsikane <jutha.se@gmail.com>
2018-02-26 14:05:33 +0700

    no message

---
+++ src/screens/Modal/notiModal.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
+import * as Animatable from 'react-native-animatable';
+import NotificationCard from '../../components/NotificationCard'
+
+export default class NotiModal extends Component {
+  constructor(props){
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+       <View style={{flex:1}}>
+            <NotificationCard />
+       </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+ 
+  
+});
